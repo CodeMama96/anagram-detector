5c772d37c845ceb557b0faf89ef71954755a44cd
Alicia Weenum <weenum.alicia@gmail.com>
2020-10-28 21:18:50 -0400

    commit tree cab2ac645cada0954dc7870419038cf11fdc4900

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+
+    end
+
+    def match(array)
+        arr = []
+        array.each do | string | 
+            if string.split("").sort == @word.split("").sort
+            arr << string
+            end
+        end
+        arr
+        #.length and #character  %w()
+    end
+end 
 # Your code goes here!
+
+#takes in no matches
